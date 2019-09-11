274d6bad8ce9134206e06d37e8a69aa7d72a48f0
Somyanath Mohanty <mohantysomyanath@gmail.com>
2019-09-11 20:43:34 +0530

    created the basic todo app without animations

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Todos from './Todos';
+import AddTodo from './AddTodo';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      { id: 1, content: 'pick up kids' },
+      { id: 2, content: 'buy groceries' }
+    ]
+  }
+  deleteTodo = (id) => {
+    const newTodoList = this.state.todos.filter(todo => {
+      return todo.id !== id;
+    });
+    this.setState({
+      todos: newTodoList
+    });
+  }
+  addTodo = (content) => {
+    const id = Math.random();
+    let newTodo = { id: id, content: content }
+    let newTodos = [...this.state.todos, newTodo];
+    this.setState({
+      todos: newTodos
+    })
+  }
+  render() {
+    return (
+      <div className="todo-app container">
+        <h1 className="center blue-text">Todo's</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}></Todos>
+        <AddTodo addTodo={this.addTodo}></AddTodo>
+      </div>
+    )
+  }
 }
 
 export default App;
