d4cd0f015d78cd828ff7538fcc59217450d02a1a
Somyanath Mohanty <mohantysomyanath@gmail.com>
2019-09-25 01:59:07 +0530

    Updates

---
+++ precache-manifest.ee6047a816007fd80f2f18811dd1ca5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3421fde49520d911e68b3659f999d1a3",
+    "url": "/todoist-app/index.html"
+  },
+  {
+    "revision": "3b62e8df4be4205f7907",
+    "url": "/todoist-app/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "9510defb581c16c38837",
+    "url": "/todoist-app/static/js/2.7b49f569.chunk.js"
+  },
+  {
+    "revision": "3b62e8df4be4205f7907",
+    "url": "/todoist-app/static/js/main.2b5ddfb6.chunk.js"
+  },
+  {
+    "revision": "c38c6eb26684af6c6086",
+    "url": "/todoist-app/static/js/runtime~main.e33bfef5.js"
+  }
+]);
